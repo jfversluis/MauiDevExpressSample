eef3a6ebe8041623f46090eb80f72d4f708a545c
Gerald Versluis <gerald.versluis@microsoft.com>
2023-07-05 15:41:29 +0200

    Initial commit

---
+++ MauiDevExpressSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDevExpressSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiDevExpressSample
+ Sample code to demonstrate how to get started with the free DevExpress controls for .NET MAUI
